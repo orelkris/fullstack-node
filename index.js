e42f9d8dda5bea6e5bb32ac8d7ae01ebf2c40cb1
orelkris <krissyorel@hotmail.com>
2024-04-27 17:40:31 -0400

    Add solution to exercises 3.1-3.8

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.use(express.json());
+
+app.use(morgan("tiny"));
+
+app.use(
+  morgan(function (tokens, req, res) {
+    if (tokens.method(req, res) === "POST") {
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        JSON.stringify(req.body),
+      ].join(" ");
+    }
+  })
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const checkNameExists = (name, persons) => {
+  return persons.some((person) => person.name === name);
+};
+
+app.get("/info", (req, res) => {
+  const timestamp = new Date();
+  const infoAmount = persons.length;
+
+  res.send(`<p>Phonebook has info for ${infoAmount} people</p>
+  <p>${timestamp}</p>
+  `);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    res.status(404).end();
+  }
+
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const id = generateId(1, 1000000);
+  const { name, number } = req.body;
+
+  if (!name) {
+    res.status(400).json({ error: "missing name" });
+  } else if (checkNameExists(name, persons)) {
+    res.status(400).json({ error: `name ${name} already in phonebook` });
+  } else if (!number) {
+    res.status(400).json({ error: "missing number" });
+  } else {
+    const person = { name, number, id: generateId(1, 100000) };
+
+    persons = [...persons, person];
+
+    res.json(person);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
